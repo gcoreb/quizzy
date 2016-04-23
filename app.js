e30d0f844ec24d29ec652cec5d5d256e6ab3cd1b
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-22 23:35:45 -0700

    updates

--- app.js
+++ app.js
@@ -1,8 +1,139 @@
 $(function(){
 
+$("#recognizeButton").click(function(){
+	
+	convertToCanvas("photo.jpg");
+	var canvas = document.getElementById('canvas2');
+            Tesseract.recognize(canvas, {lang: 'eng'}).then(function (d) {
+                console.log(d.text)
+            }, function (err) {
+                console.log(err);
+            });
+});
+
 $("#userInfoButton").click(function(){
 	submitUserInfo();
 });
+
+var accessToken;
+
+// if(window.location.href.indexOf("code")!=-1){
+// 	continueQuizletAuth();
+// }
+
+
+  function convertToCanvas (lastPhoto) {
+
+
+        var canvas2 = document.getElementById("canvas2");
+
+        canvas2.width = lastPhoto.width;
+
+        canvas2.height = lastPhoto.height;
+
+        var canvasbanana = canvas2.getContext("2d");
+
+        var img = new Image();
+        img.src = "photo.jpg";
+        img.width = "1000";
+        img.height="1000";
+        canvas2.width = img.width;
+        canvas2.height = img.height;
+        console.log(img.width + " " + img.height);
+        img.onload = function() {
+            canvasbanana.drawImage(img, 0,0);
+        }
+
+        return canvasbanana;
+    }
+
+	function continueQuizletAuth() {
+		var currentURL = window.location.href;
+		var code = currentURL.substring(currentURL.indexOf("code=")+5);
+
+		// var xhrRequest = function (url, type, callback) {
+		// 	var xhr = new XMLHttpRequest();
+		//   	xhr.onload = function () {
+		//     	callback(this.responseText);
+		//   	};
+		//   	xhr.open(type, url, true);
+		//   	xhr.setRequestHeader("Authorization", "Basic 4msU8P4c2B:NG1zVThQNGMyQjpjbVRYeXB1N1FZcFUzN2NTYnp1ejJI");
+		//   	xhr.setRequestHeader("Content-Type", "application/jsonp; charset=UTF-8");
+		//   	xhr.send();
+
+		// }
+
+		// var url ="https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/index.html";
+		
+		// xhrRequest(url, "POST", function(resp){
+		// 	var jsonversion = form2Json(resp);
+		// 	accessToken = data.access_token;
+		// });
+		alert('about to post req');
+		var url ="https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/index.html";
+		 $.ajax({
+            type:"POST",
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("Authorization", "Basic NG1zVThQNGMyQjpjbVRYeXB1N1FZcFUzN2NTYnp1ejJI");
+                //request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+            },
+            url: url,
+            success: function(msg) {
+                console.log(msg);
+            },
+            error:function(error){
+            	console.log(error);
+            }
+    });
+
+	}
+
+	function form2Json(str)
+	{
+	    var obj,i,pt,keys,j,ev;
+	    if (typeof form2Json.br !== 'function')
+	    {
+	        form2Json.br = function(repl)
+	        {
+	            if (repl.indexOf(']') !== -1)
+	            {
+	                return repl.replace(/\](.+?)(,|$)/g,function($1,$2,$3)
+	                {
+	                    return form2Json.br($2+'}'+$3);
+	                });
+	            }
+	            return repl;
+	        };
+	    }
+	    str = '{"'+(str.indexOf('%') !== -1 ? decodeURI(str) : str)+'"}';
+	    obj = str.replace(/\=/g,'":"').replace(/&/g,'","').replace(/\[/g,'":{"');
+	    obj = JSON.parse(obj.replace(/\](.+?)(,|$)/g,function($1,$2,$3){ return form2Json.br($2+'}'+$3);}));
+	    pt = ('&'+str).replace(/(\[|\]|\=)/g,'"$1"').replace(/\]"+/g,']').replace(/&([^\[\=]+?)(\[|\=)/g,'"&["$1]$2');
+	    pt = (pt + '"').replace(/^"&/,'').split('&');
+	    for (i=0;i<pt.length;i++)
+	    {
+	        ev = obj;
+	        keys = pt[i].match(/(?!:(\["))([^"]+?)(?=("\]))/g);
+	        for (j=0;j<keys.length;j++)
+	        {
+	            if (!ev.hasOwnProperty(keys[j]))
+	            {
+	                if (keys.length > (j + 1))
+	                {
+	                    ev[keys[j]] = {};
+	                }
+	                else
+	                {
+	                    ev[keys[j]] = pt[i].split('=')[1].replace(/"/g,'');
+	                    break;
+	                }
+	            }
+	            ev = ev[keys[j]];
+	        }
+	    }
+	    return obj;
+	}
 
 	function submitUserInfo() {
 		var username = $("#username").val();
@@ -17,14 +148,10 @@ $("#userInfoButton").click(function(){
 
 	function quizletAuth(username, pass) {
 		var str = makeid();
-		var redirectURI = "https://quizlet.com/authorize?response_type=code&client_id=4msU8P4c2B&scope=write_set&state="+str;
-		//alert(redirectURI);
-		window.open(redirectURI,'auth time');
+		var redirectURI = "https://quizlet.com/authorize?response_type=code&client_id=4msU8P4c2B&scope=write_set&state="+str;		
 		var currentURL = window.location.href;
-		//alert(currentURL);
-		waitForUrlToChangeTo("code=");
-		alert(window.location.href);
-
+		console.log(currentURL.indexOf("code"));
+		window.open(redirectURI,'auth time');
 	}
 
 
