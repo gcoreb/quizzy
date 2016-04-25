2c973c54d7ee395ff6d495d1b6b09501a0c8d402
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-25 00:14:47 -0700

    fkignfinally

--- server/app.js
+++ server/app.js
@@ -1,30 +1,35 @@
-var http = require('http');
-//var request = require('request');
-var requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!\n');
-  //console.log(req);
-}
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+var request = require('request');
 
-// var doReq = function(code) {
-// request({
-//     url: 'https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/analyze.html',
-//     method: 'POST', 
-//     headers: { 
-//         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-//         'Authorization': 'Basic NG1zVThQNGMyQjpjbVRYeXB1N1FZcFUzN2NTYnp1ejJI'
-//     }
-// }, function(error, response, body){
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(response.statusCode, body);
-//     }
-// });
-// }
+app.get('/quizlet', function(req, res){
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+	var realCode = req.query.code;
+	res.send('code: ' + req.query.code);
+	request({
+	    url: 'https://api.quizlet.com/oauth/token?grant_type=authorization_code&code='+realCode+'&redirect_uri=http://kushaltirumala.github.io/quizzy/analyze.html', 
+	    method: 'POST', 
+	    headers: { 
+	        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	        'Authorization': 'Basic NG1zVThQNGMyQjpjbVRYeXB1N1FZcFUzN2NTYnp1ejJI'
+	    }
+	}, function(error, response, body){
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	        //var jsonbody = JSON.parse(body);
+	        console.log("acces token for bryan chen:" + JSON.parse(body).access_token);
+	        
 
+	    }
+	});
 
-var server = http.createServer(requestListener);
-server.listen(8080, function() {
-	console.log('listenin on port 8080');
 });
+
+
+app.listen(3000);
