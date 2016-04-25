e5aa73889311564243d916929154b2bffc3dcb89
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-25 16:59:37 -0700

    proto

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express    = require('express');
 var app        = express();                 
 var bodyParser = require('body-parser');
 var request = require('request');
-
+const acstkn;
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -29,10 +29,23 @@ app.get('/quizlet', function(req, res){
 	        console.log(response.statusCode, body);
 	        var acstoken = JSON.parse(body).access_token;
 	        console.log("access token for bryan chen:" + acstoken);
+	        acstkn=acstoken;
 	        var resp = {daisytodd: acstoken};
 	        res.send(resp);
 	    }
 	});
+});
+
+app.get('sendQuizletSet', function(req, res){
+	var title = req.query.title;
+	var terms;
+	var defs;
+	var langterms = "en";
+	var langdefs = "en";
+	var potHeader = "Authorization: Bearer " + acstkn;
+	var url ="https://api.quizlet.com/2.0/sets?title="+title+"&terms=[" + terms  +"]&definitions=["+ defs + "]&lang_terms="+langterms+"&lang_definitions="+lang_defs;
+
+	//post request with new set here
 });
 
 app.listen(3000);
