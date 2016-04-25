29822623f835c1d3a43ba6fb16572419f6e59663
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-24 22:33:04 -0700

    recreate

--- server/app.js
+++ server/app.js
@@ -1,13 +1,30 @@
 var http = require('http');
-
-const PORT=8080; 
-
-function handleRequest(request, response){
-    console.log('handling a new request');
+//var request = require('request');
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+  //console.log(req);
 }
 
-var server = http.createServer(handleRequest);
+// var doReq = function(code) {
+// request({
+//     url: 'https://api.quizlet.com/oauth/token?grant_type=authorization_code&code="+code+"&redirect_uri=http://kushaltirumala.github.io/quizzy/analyze.html',
+//     method: 'POST', 
+//     headers: { 
+//         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+//         'Authorization': 'Basic NG1zVThQNGMyQjpjbVRYeXB1N1FZcFUzN2NTYnp1ejJI'
+//     }
+// }, function(error, response, body){
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(response.statusCode, body);
+//     }
+// });
+// }
 
-server.listen(PORT, function(){
-    console.log("listenig on ", PORT);
+
+var server = http.createServer(requestListener);
+server.listen(8080, function() {
+	console.log('listenin on port 8080');
 });
